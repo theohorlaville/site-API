33c4ce2cd47ddae3f040515f4b206e1551e161a1
theohorlaville <theo.horlaville76@orange.fr>
2020-12-03 19:17:54 +0100

    SITEBGSAMERE

--- src/script.js
+++ src/script.js
@@ -7,12 +7,19 @@ const retour=document.querySelector('#retour');
 const form=document.querySelector('form');
 const titreOeuvre=document.querySelector('#titre_oeuvre');
 const auteur=document.querySelector('#auteur');
+const bas= document.querySelector('#bas');
+var lyrics=document.createElement("p");
+var request = new XMLHttpRequest();
 
-affiche.addEventListener('click',function(){
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
     sect1.style.width=0+'vw';
     sect2.style.width=100+'vw';
 
     parole();
+
+    
 
 });
 
@@ -25,19 +32,27 @@ const titre=form.titre.value;
 titreOeuvre.innerHTML=titre;
 auteur.innerHTML=artiste;
 
-var request = new XMLHttpRequest();
+
 request.open('GET', 'https://api.lyrics.ovh/v1/'+artiste+'/'+titre);
 
 request.onreadystatechange = function () {
   if (this.readyState === 4) {
     var reponse= JSON.parse(this.responseText);
-    var lyrics=document.createElement("p");
-    lyrics.textContent=reponse.lyrics;
-    sect2.appendChild(lyrics);
+    if(reponse.lyrics===''){
+      lyrics.innerHTML="Pas de parole trouvée pour "+titre+" de "+artiste;
+      sect2.insertBefore(lyrics,bas);
+    }
+
+    else{
+    lyrics.classList.add("parole");
+    lyrics.innerHTML=reponse.lyrics;
+    sect2.insertBefore(lyrics,bas);
     console.log(this.responseText);
+    }
   }
 };
 request.send();
+
 
 
 
@@ -48,7 +63,15 @@ request.send();
 retour.addEventListener('click',function(){
     sect1.style.width=100+'vw';
     sect2.style.width=0+'vw';
+    lyrics.innerHTML='';
+    form.titre.value='';
+    form.artiste.value='';
+    titreOeuvre.innerHTML='';
+    auteur.innerHTML="";
+
+
 })
     
 
 })
+
